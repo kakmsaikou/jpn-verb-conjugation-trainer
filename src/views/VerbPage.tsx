a7822adaaa1e419b3ce05bb3ce8d6cf6fa2c8656
TacitusPie <kakmsaikou@gmail.com>
2023-03-12 19:12:49 +0800

    feat: 完成 HTML 基本布局

--- src/views/VerbPage.tsx
+++ src/views/VerbPage.tsx
@@ -6,8 +6,9 @@ export const VerbPAge = defineComponent({
     return () => <div class={s.wrapper}>
       <h1>食べる</h1>
       <form action="">
-        <div class={s.radioList}>
-          <label>
+        <ul>
+          <li class={s.radioList}>
+                      <label>
             <input type="radio" value='一类动词' name='wordType'/>
             <span>一类动词</span>
           </label>
@@ -19,7 +20,29 @@ export const VerbPAge = defineComponent({
             <input type="radio" value='三类动词' name='wordType'/>
             <span>三类动词</span>
           </label>
+          </li>
+        </ul>
+        <div>
+          <span>ます形：</span>
+          <input type="text" />
+          <span>答案：<em>食べます</em></span>
         </div>
+        <div>
+          <span>　て形：</span>
+          <input type="text" />
+          <span>答案：<em>食べて</em></span>
+        </div>
+        <div>
+          <span>　た形：</span>
+          <input type="text" />
+          <span>答案：<em>食べた</em></span>
+        </div>
+        <div>
+          <span>ない形：</span>
+          <input type="text" />
+          <span>答案：<em>食べない</em></span>
+        </div>
+        <button type='submit'>提交</button>
       </form>
     </div>;
   },
