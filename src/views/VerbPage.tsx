01f6f83e8e388c33728a2f3bcb274f8b5fa4bc6c
TacitusPie <kakmsaikou@gmail.com>
2023-03-12 18:59:54 +0800

    feat: 创建单词类型框

--- src/views/VerbPage.tsx
+++ src/views/VerbPage.tsx
@@ -3,7 +3,25 @@ import s from './VerbPAge.module.scss';
 
 export const VerbPAge = defineComponent({
   setup: (props, context) => {
-    return () => <div>VerbPAge</div>;
+    return () => <div class={s.wrapper}>
+      <h1>食べる</h1>
+      <form action="">
+        <div class={s.radioList}>
+          <label>
+            <input type="radio" value='一类动词' name='wordType'/>
+            <span>一类动词</span>
+          </label>
+          <label>
+            <input type="radio" value='二类动词' name='wordType'/>
+            <span>二类动词</span>
+          </label>
+          <label>
+            <input type="radio" value='三类动词' name='wordType'/>
+            <span>三类动词</span>
+          </label>
+        </div>
+      </form>
+    </div>;
   },
 });
 
