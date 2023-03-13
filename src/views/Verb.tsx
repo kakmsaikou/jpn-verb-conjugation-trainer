8cdb0a47bc0165052d222eadbfcc41512593c707
TacitusPie <kakmsaikou@gmail.com>
2023-03-13 15:05:25 +0800

    feat: 能够监听结果打印不同的 log 了

--- src/views/Verb.tsx
+++ src/views/Verb.tsx
@@ -1,8 +1,18 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import s from './Verb.module.scss';
+import { withEventModifiers } from '../plugins/withEventmodifiers';
 
 export const Verb = defineComponent({
   setup: (props, context) => {
+    const refCorrectAnswer = ref('やります');
+    const handleInput = (e: any) => {
+      const answer = e.srcElement.value;
+      if (answer === refCorrectAnswer.value) {
+        console.log('correct');
+      } else {
+        console.log('wrong');
+      }
+    };
     return () => (
       <div class={s.wrapper}>
         <h1>日语词汇变形练习</h1>
@@ -25,7 +35,16 @@ export const Verb = defineComponent({
             <h3 class={s.questionContent}>ます形</h3>
             <p class={s.resultMessage}>やります</p>
           </div>
-          <input class={s.answer} type='text' />
+          <input
+            type='text'
+            class={s.answer}
+            {...withEventModifiers(
+              {
+                onKeyup: handleInput,
+              },
+              ['enter']
+            )}
+          />
           <div class={s.settingWrapper}>
             <span class={s.continue}>单击 Enter 继续</span>
           </div>
