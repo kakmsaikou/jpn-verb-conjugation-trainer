9e2595074866d6e4d5dc3e431d20a69810562a71
TacitusPie <kakmsaikou@gmail.com>
2023-03-19 21:31:47 +0800

    chore: 优化了点逻辑

--- src/utils/getVoices.ts
+++ src/utils/getVoices.ts
@@ -3,29 +3,29 @@ import { getKey } from './getKey';
 
 const configStore = useConfigStore();
 
-export const getVoices = (posStr: Pos, form: WordForm, voices: Voices, type: WordType) => {
-  if (posStr === 'verb') {
+export const getVoices = (pos: Pos, form: WordForm, voices: Voices, type: WordType) => {
+  if (pos === 'verb') {
     getVerbVoices(form, voices);
-  } else if (posStr === 'adj') {
-    avoidPurePlain(voices, configStore.tempConfig.adj!, type);
+  } else if (pos === 'adj') {
+    avoidPurePlainAdj(voices, configStore.tempConfig.adj!, type);
   }
 };
 
 const getVerbVoices = (form: WordForm, voices: Voices) => {
-  voices.present = false;
   switch (form) {
     case 'plain':
       {
         const { polarity, tense } = configStore.tempConfig.verb!;
-        voices.polite = false;
-        setNegAndPresForPlain(voices, polarity, tense);
+        form === 'plain' ? (voices.polite = false) : (voices.polite = true);
+        avoidPurePlainVerb(voices, polarity, tense);
       }
       break;
     case 'masu':
       {
         const { polarity, tense } = configStore.tempConfig.verb!;
         voices.polite = true;
-        setNegAndPres(voices, polarity, tense);
+        voices.negative = getKey(polarity) === 'negative';
+        voices.present = getKey(tense) === 'present';
       }
       break;
     case 'te':
@@ -35,30 +35,24 @@ const getVerbVoices = (form: WordForm, voices: Voices) => {
   }
 };
 
+// 用于动词
+const avoidPurePlainVerb = (voices: Voices, polarity: Record<Polarity, boolean>, tense: Record<Tense, boolean>) => {
+  voices.negative = getKey(polarity) === 'negative';
+  voices.present = getKey(tense) === 'present';
+  if (!voices.negative && voices.present) {
+    avoidPurePlainVerb(voices, polarity, tense);
+  }
+};
+
 // 用于形容词
-const avoidPurePlain = (voices: Voices, voiceConfig: VoicesConfig, type: WordType) => {
+const avoidPurePlainAdj = (voices: Voices, voiceConfig: VoicesConfig, type: WordType) => {
   const { sow, polarity, tense } = voiceConfig;
   voices.polite = getKey(sow) === 'polite';
-  setNegAndPres(voices, polarity, tense);
-  
-  // 防止出现「简体 + 肯定 + 现在」的结果
-  if (!voices.polite && type !== 'adj_na') {
-    if (!voices.negative && voices.present) {
-      avoidPurePlain(voices, voiceConfig, type);
-    }
-  }
-};
-
-// 用于动词
-const setNegAndPres = (voices: Voices, polarity: Record<Polarity, boolean>, tense: Record<Tense, boolean>) => {
   voices.negative = getKey(polarity) === 'negative';
   voices.present = getKey(tense) === 'present';
-};
 
-const setNegAndPresForPlain = (voices: Voices, polarity: Record<Polarity, boolean>, tense: Record<Tense, boolean>) => {
-  voices.negative = getKey(polarity) === 'negative';
-  voices.present = getKey(tense) === 'present';
-  if(!voices.negative && voices.present) {
-    setNegAndPresForPlain(voices, polarity, tense);
+  // 防止出现「イ形 + 简体 + 肯定 + 现在」的结果
+  if (type === 'adj_i' && !voices.polite && !voices.negative && voices.present) {
+    avoidPurePlainAdj(voices, voiceConfig, type);
   }
-}
+};
