795a1dd355af72a6f27b6d08580835b2a5954d95
TacitusPie <kakmsaikou@gmail.com>
2023-03-19 12:47:24 +0800

    refactor: 把 handleCheckbox 改为匿名函数

--- src/utils/handleCheckbox.ts
+++ src/utils/handleCheckbox.ts
@@ -1,24 +1,23 @@
+type VoiceRecord = Record<Sow, boolean> | Record<Tense, boolean> | Record<Polarity, boolean>;
+
 const sow: [string, string] = ['plain', 'polite'];
 const tense: [string, string] = ['present', 'past'];
 const polarity: [string, string] = ['affirmative', 'negative'];
 
-function handleCheckbox(voice: Record<Sow, boolean>, key: Sow): void;
-function handleCheckbox(voice: Record<Tense, boolean>, key: Tense): void;
-function handleCheckbox(voice: Record<Polarity, boolean>, key: Polarity): void;
-function handleCheckbox(voice: any, key: any): void {
+const autoCheck = (voice: any, key: any, voiceList: [string, string]) => {
+  if (!voice[voiceList[0]] === !voice[voiceList[1]]) {
+    const target = voiceList[1 - voiceList.indexOf(key)];
+    voice[target] = true;
+  }
+};
+
+export const handleCheckbox = (voice: VoiceRecord, key: keyof VoiceRecord): void => {
   if (sow.includes(key)) {
     autoCheck(voice, key, sow);
   } else if (tense.includes(key)) {
     autoCheck(voice, key, tense);
   } else if (polarity.includes(key)) {
     autoCheck(voice, key, polarity);
-  }
-}
-
-const autoCheck = (voice: any, key: any, voiceList: [string, string]) => {
-  if (!voice[voiceList[0]] === !voice[voiceList[1]]) {
-    const target = voiceList[1 - voiceList.indexOf(key)];
-    voice[target] = true;
   }
 };
 
