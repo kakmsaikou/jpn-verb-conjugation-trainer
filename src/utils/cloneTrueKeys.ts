4f37dee82f024c3ba7b8886bd46a189319cf6152
TacitusPie <kakmsaikou@gmail.com>
2023-03-24 10:00:04 +0800

    chore: 类型改为 any 防止报错

--- src/utils/cloneTrueKeys.ts
+++ src/utils/cloneTrueKeys.ts
@@ -1,15 +1,11 @@
-type RecursiveObject = {
-  [key: string]: boolean | RecursiveObject;
-};
-
 // 这个函数用于克隆一个对象，只克隆对象中值为true的属性
-export const cloneTrueKeys = (obj: RecursiveObject): RecursiveObject => {
-  const newObj: RecursiveObject = {};
-  const traverse = (source: RecursiveObject, clone: RecursiveObject) => {
+export const cloneTrueKeys = (obj: any): any => {
+  const newObj = {};
+  const traverse = (source: any, clone: any) => {
     for (const key in source) {
       if (typeof source[key] === 'object' && source[key] !== null) {
         clone[key] = {};
-        traverse(source[key] as RecursiveObject, clone[key] as RecursiveObject);
+        traverse(source[key], clone[key]);
       } else if (source[key] === true) {
         clone[key] = true;
       }
