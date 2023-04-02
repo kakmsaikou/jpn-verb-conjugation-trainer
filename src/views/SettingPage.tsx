fe6620c5c944fc9d158bcdf2bce9d4b9171f884d
TacitusPie <kakmsaikou@gmail.com>
2023-04-02 14:44:42 +0800

    chore: 准备引入 element UI

--- src/views/SettingPage.tsx
+++ src/views/SettingPage.tsx
@@ -64,10 +64,12 @@ export const SettingPage = defineComponent({
             <input type="checkbox" v-model={tempConfig.voice}/>
             是否发音？
           </p>
+          
           <p class={s.target}>
-            每日目标（达到目标后会提醒）
+            每日答题目标（达到目标后会提醒）
             <input type='number' class={s.targetNum} value={0} min={0}/>
           </p>
+
           <div class={s.relativeBox}>
             <h4 v-show={!posValid.value}>*你至少需要选择一个类别</h4>
             <VerbOptions tempConfig={tempConfig} onUpdateVerb={updateVerbValid} />
