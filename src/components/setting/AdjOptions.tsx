6496d987f5afc4b6ca8aecf2628878086aa4ab6b
TacitusPie <kakmsaikou@gmail.com>
2023-04-02 11:36:51 +0800

    refactor: 重构 AdjOptions

--- src/components/setting/AdjOptions.tsx
+++ src/components/setting/AdjOptions.tsx
@@ -1,6 +1,5 @@
-import { computed, defineComponent, PropType, Ref, watch } from 'vue';
-import { ADJ_TENSE_LIST, ADJ_TYPE_LIST, BILINGUAL_LIST, POLARITY_LIST, SOW_LIST, TENSE_LIST } from '../../const';
-import { handleCheckbox } from '../../utils/handleCheckbox';
+import { computed, defineComponent, PropType, watch } from 'vue';
+import { ADJ_TENSE_LIST, ADJ_TYPE_LIST, BILINGUAL_LIST } from '../../const';
 import s from './WordOptions.module.scss';
 
 export const AdjOptions = defineComponent({
@@ -14,26 +13,22 @@ export const AdjOptions = defineComponent({
   setup: (props, context) => {
     const { pos, adj } = props.tempConfig;
 
-    const adjVoiceList = [SOW_LIST, TENSE_LIST, POLARITY_LIST];
-
     const typeValid = computed(() => {
       return adj.adj_i || adj.adj_na;
     });
-    const plainValid = computed(() => {
-      return !(
-        adj.adj_i &&
-        !adj.adj_na &&
-        adj.plain &&
-        !adj.polite &&
-        adj.affirmative &&
-        !adj.negative &&
-        adj.present &&
-        !adj.past
-      );
+
+    const tenseValid = computed(() => {
+      let n = 0;
+      ADJ_TENSE_LIST.forEach(tense => {
+        if (adj[tense] === true) n++;
+      });
+      return n > 0 ? true : false;
     });
+
     const adjValid = computed(() => {
-      return plainValid.value && typeValid.value;
+      return tenseValid.value && typeValid.value;
     });
+
     watch(adjValid, newVal => {
       context.emit('updateAdj', newVal);
     });
@@ -47,7 +42,6 @@ export const AdjOptions = defineComponent({
           <>
             <div class={s.relativeBox}>
               <h4 v-show={!typeValid.value}>*你至少需要选择一个类别</h4>
-              <h4 v-show={!plainValid.value}>*你不能同时只选择“イ形容词”、“基本形 / 简体”、“现在”、“肯定”</h4>
               <ul>
                 {ADJ_TYPE_LIST.map(type => (
                   <li>
@@ -57,7 +51,8 @@ export const AdjOptions = defineComponent({
                 ))}
               </ul>
             </div>
-            <div>
+            <div class={s.relativeBox}>
+              <h4 v-show={!tenseValid.value}>*你至少需要选择一个类别</h4>
               <ul>
                 {ADJ_TENSE_LIST.map(tense => {
                   return (
@@ -68,28 +63,6 @@ export const AdjOptions = defineComponent({
                   );
                 })}
               </ul>
-            </div>
-            <div class={s.relativeBox}>
-              {adjVoiceList.map(item => {
-                return (
-                  <ul>
-                    {item.map(key => {
-                      return (
-                        <li>
-                          <input
-                            type='checkbox'
-                            v-model={adj[key]}
-                            onChange={() => {
-                              handleCheckbox(adj, key);
-                            }}
-                          />
-                          <span>{BILINGUAL_LIST[key]}</span>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                );
-              })}
             </div>
           </>
         ) : null}
