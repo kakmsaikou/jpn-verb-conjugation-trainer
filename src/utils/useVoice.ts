62b91f7f70545af393e5a8cc9bc7129d891f1f10
TacitusPie <kakmsaikou@gmail.com>
2023-04-02 13:29:22 +0800

    feat: 优化音频加载

--- src/utils/useVoice.ts
+++ src/utils/useVoice.ts
@@ -4,16 +4,16 @@ const configStore = useConfigStore();
 export const useVoice = () => {
   const utterance = new SpeechSynthesisUtterance();
   utterance.lang = 'ja-JP';
-
-  window.speechSynthesis.onvoiceschanged = function () {
-    const voice = speechSynthesis.getVoices().find(voice => voice.name === 'Google 日本語');
-    console.log(voice);
-    if (voice) {
-      utterance.voice = voice;
-    }
-  };
+  let count = 0;
 
   const speak = (text: string) => {
+    if(count === 0){
+      const voice = speechSynthesis.getVoices().find(voice => voice.name === 'Google 日本語');
+      if (voice) {
+        utterance.voice = voice;
+        count++
+      }
+    }
     if (configStore.config.voice === true) {
       utterance.text = text;
       speechSynthesis.speak(utterance);
